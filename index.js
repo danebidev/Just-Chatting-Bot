af2be89a9bd0609f02c23f20c882cb612988ee29
BuildTools <danebi5606@gmail.com>
2022-02-07 17:33:12 +0100

    test

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const suffix = '!';
+const { suffix } = require('./config.json');
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const token = process.env.token;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+client.login(token);
+
+client.addListener("")
