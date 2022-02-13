11181ee39e94e8f846ad43be8c0d805deed09599
BuildTools <danebi5606@gmail.com>
2022-02-13 23:11:52 +0100

    Small changes

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ dotenv.config();
 const token = process.env.TOKEN;
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES], partials: ['CHANNEL'] });
 client.config = config;
-client.exists = fs.existsSync('./bumps.json');
 registerEvents();
 readCommands();
 
 // Random stuff
-if(!client.exists) {
+if(!fs.existsSync('./bumps.json')) {
 	fs.writeFile('./bumps.json', JSON.stringify({}, null, 2), err => {
 		if(err) return console.error(err);
 	});
