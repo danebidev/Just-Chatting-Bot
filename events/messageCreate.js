4db05c081fe1ccb83a7d81dbecb965d6e2ac9457
BuildTools <danebi5606@gmail.com>
2022-02-14 17:50:06 +0100

    Added role check

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,13 +4,19 @@ module.exports = {
 
 	name: 'messageCreate',
 
-	execute: function(message, client) {
+	execute: async function(message, client) {
 
 		if(message.author.id == '302050872383242240') return bumpCount.execute(message, client);
 
 		message.content = message.content.trim();
 
 		if(message.content.substring(0, client.config.prefix.length) != client.config.prefix || message.author.bot) return;
+
+		const guild = await client.guilds.cache.get('917119141511589959');
+		const role = await guild.roles.cache.get('942820621744742432');
+		const member = await guild.members.cache.get(message.author);
+
+		if(!member.roles.cache.has(role.id)) return;
 
 		const args = message.content.split(/\s+/);
 		const commandName = args.shift().substring(client.config.prefix.length).toLowerCase();
