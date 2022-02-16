a3f77f10a68bc4989bbe04fce64f813e4e048ff9
BuildTools <danebi5606@gmail.com>
2022-02-16 18:28:40 +0100

    Added pg module and database pool

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 // Imports
 const config = require('./config.json');
+<<<<<<< Updated upstream
 const fs = require('fs');
 const dotenv = require('dotenv');
+=======
+>>>>>>> Stashed changes
 const Discord = require('discord.js');
+const pg = require('pg');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 
+<<<<<<< Updated upstream
 // Other constants
 dotenv.config();
+=======
+// Variables / Properties
+>>>>>>> Stashed changes
 const token = process.env.TOKEN;
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES], partials: ['CHANNEL'] });
 client.config = config;
-registerEvents();
-readCommands();
+client.bumps = require('./bumps.json');
+client.database = new pg.Pool({
+	connectionString: process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/postgres',
+	ssl: process.env.DATABASE_URL ? {
+		rejectUnauthorized: false
+	} : false
+});
 
 
 // Random shit
-client.bumps = require('./bumps.json');
+registerEvents();
+readCommands();
+
 client.login(token);
+client.database.connect();
 
 
 // Functions
